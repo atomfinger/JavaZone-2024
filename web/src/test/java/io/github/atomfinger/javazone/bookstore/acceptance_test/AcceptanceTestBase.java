b166f4340d6eb099e9a02a34ebd5c48a002a158c
John M. Lindbakk <john@lindbakk.com>
2024-07-02 15:00:26 +0200

    Starting to add mockserver support

--- web/src/test/java/io/github/atomfinger/javazone/bookstore/acceptance_test/AcceptanceTestBase.java
+++ web/src/test/java/io/github/atomfinger/javazone/bookstore/acceptance_test/AcceptanceTestBase.java
@@ -11,39 +11,53 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.MockServerContainer;
+import org.mockserver.client.server.MockServerClient;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTestBase {
 
-    @Container
-    public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:15-alpine").withDatabaseName(
-            "testdb").withUsername("test").withPassword("test");
-    @Autowired
-    public TestRestTemplate restTemplate;
-    @LocalServerPort
-    private int port;
-    @Autowired
-    private Flyway flyway;
+  @Container
+  public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:15-alpine").withDatabaseName(
+      "testdb").withUsername("test").withPassword("test");
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresDB::getUsername);
-        registry.add("spring.datasource.password", postgresDB::getPassword);
-        registry.add("spring.flyway.cleanDisabled", () -> false);
-    }
+  public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
+      .parse("mockserver/mockserver")
+      .withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
 
-    @BeforeEach
-    public void resetDb() {
-        flyway.clean();
-        flyway.migrate();
-    }
+  @Container
+  public MockServerContainer mockServer = new MockServerContainer(MOCKSERVER_IMAGE);
 
-    /**
-     * Creates the URL for the service running locally.
-     */
-    public String getUrl() {
-        return "http://localhost:" + port + "/api";
-    }
+  @Autowired
+  public TestRestTemplate restTemplate;
+
+  public static MockServerClient mockServerClient = new MockServerClient(mockserver.getServerPort());
+
+  @LocalServerPort
+  private int port;
+  @Autowired
+  private Flyway flyway;
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+    registry.add("spring.datasource.username", postgresDB::getUsername);
+    registry.add("spring.datasource.password", postgresDB::getPassword);
+    registry.add("spring.flyway.cleanDisabled", () -> false);
+  }
+
+  @BeforeEach
+  public void resetDb() {
+    mockserverClient.reset();
+    flyway.clean();
+    flyway.migrate();
+  }
+
+  /**
+   * Creates the URL for the service running locally.
+   */
+  public String getUrl() {
+    return "http://localhost:" + port + "/api";
+  }
 }
--- web/src/test/java/io/github/atomfinger/javazone/bookstore/contract_test/Consumer1Base.java
+++ web/src/test/java/io/github/atomfinger/javazone/bookstore/contract_test/Consumer1Base.java
@@ -2,6 +2,7 @@ package io.github.atomfinger.javazone.bookstore.contract_test;
 
 import io.github.atomfinger.javazone.bookstore.bookstore.persistence.entities.Book;
 import io.github.atomfinger.javazone.bookstore.bookstore.service.BookService;
+import io.github.atomfinger.javazone.bookstore.bookstore.service.BookService.BookWithOrderNumbers;
 import io.github.atomfinger.javazone.bookstore.controller.BookController;
 import io.restassured.config.EncoderConfig;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -13,20 +14,20 @@ import java.util.List;
 
 public abstract class Consumer1Base {
 
-    BookController controller;
+  BookController controller;
 
-    @BeforeEach
-    public void setup() {
-        var book = new Book();
-        book.setTitle("Test");
-        book.setDescription("More test");
-        book.setAuthorName("John");
-        var mockedBookService = Mockito.mock(BookService.class);
-        Mockito.when(mockedBookService.listBooks()).thenReturn(List.of(book));
-        controller = new BookController(mockedBookService);
-        var encoderConfig = new EncoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false);
-        RestAssuredMockMvc.config = new RestAssuredMockMvcConfig().encoderConfig(encoderConfig);
-        RestAssuredMockMvc.standaloneSetup(this.controller);
-    }
-
+  @BeforeEach
+  public void setup() {
+    var book = new Book();
+    book.setTitle("Test");
+    book.setDescription("More test");
+    book.setAuthorName("John");
+    var bookWithOrderNums = new BookWithOrderNumbers(book, null);
+    var mockedBookService = Mockito.mock(BookService.class);
+    Mockito.when(mockedBookService.listBooks()).thenReturn(List.of(bookWithOrderNums));
+    controller = new BookController(mockedBookService);
+    var encoderConfig = new EncoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false);
+    RestAssuredMockMvc.config = new RestAssuredMockMvcConfig().encoderConfig(encoderConfig);
+    RestAssuredMockMvc.standaloneSetup(this.controller);
+  }
 }
