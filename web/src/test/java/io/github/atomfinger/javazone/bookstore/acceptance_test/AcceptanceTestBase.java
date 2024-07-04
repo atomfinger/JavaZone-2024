bf2e6c3d87d515dc89b6d68c583db6578329510e
John Mikael Lindbakk <john@jmgundersen.com>
2024-07-04 21:59:12 +0200

    Adding a little more robust setup

--- web/src/test/java/io/github/atomfinger/javazone/bookstore/acceptance_test/AcceptanceTestBase.java
+++ web/src/test/java/io/github/atomfinger/javazone/bookstore/acceptance_test/AcceptanceTestBase.java
@@ -11,57 +11,55 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MockServerContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTestBase {
 
-  @Container
-  public static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:15-alpine").withDatabaseName(
-      "testdb").withUsername("test").withPassword("test");
+    public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName.parse("mockserver/mockserver")
+            .withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
+    public static PostgreSQLContainer<?> postgresDB;
+    public static MockServerContainer mockServerContainer;
 
-  public static final DockerImageName MOCKSERVER_IMAGE = DockerImageName
-      .parse("mockserver/mockserver")
-      .withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
+    static {
+        mockServerContainer = new MockServerContainer(MOCKSERVER_IMAGE);
+        mockServerContainer.start();
+        postgresDB = new PostgreSQLContainer<>("postgres:15-alpine").withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test");
+        postgresDB.start();
+    }
 
-  @Container
-  public static MockServerContainer mockServerContainer = new MockServerContainer(MOCKSERVER_IMAGE);
+    @Autowired
+    public TestRestTemplate restTemplate;
+    public MockServerClient mockServerClient = new MockServerClient("localhost", mockServerContainer.getServerPort());
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private Flyway flyway;
 
-  @Autowired
-  public TestRestTemplate restTemplate;
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDB::getUsername);
+        registry.add("spring.datasource.password", postgresDB::getPassword);
+        registry.add("spring.flyway.cleanDisabled", () -> false);
+        registry.add("api.order-endpoint", () -> "http://localhost:" + mockServerContainer.getServerPort() + "/order");
+        registry.add("api.inventory-endpoint", () -> "http://localhost:" + mockServerContainer.getServerPort() + "/inventory");
+        registry.add("api.best-reads-endpoint", () -> "http://localhost:" + mockServerContainer.getServerPort() + "/best-reads");
+    }
 
-  public MockServerClient mockServerClient = new MockServerClient("localhost", mockServerContainer.getServerPort());
+    @BeforeEach
+    public void reset() {
+        mockServerClient.reset();
+        flyway.clean();
+        flyway.migrate();
+    }
 
-  @LocalServerPort
-  private int port;
-  @Autowired
-  private Flyway flyway;
-
-  @DynamicPropertySource
-  static void configureProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
-    registry.add("spring.datasource.username", postgresDB::getUsername);
-    registry.add("spring.datasource.password", postgresDB::getPassword);
-    registry.add("spring.flyway.cleanDisabled", () -> false);
-    registry.add("api.order-endpoint", () -> "http://localhost:" + mockServerContainer.getServerPort() + "/order");
-    registry.add("api.inventory-endpoint", () -> "http://localhost:" + mockServerContainer.getServerPort() + "/inventory");
-    registry.add("api.best-reads-endpoint", () -> "http://localhost:" + mockServerContainer.getServerPort() + "/best-reads");
-  }
-
-  @BeforeEach
-  public void resetDb() {
-    mockServerClient.reset();
-    flyway.clean();
-    flyway.migrate();
-  }
-
-  /**
-   * Creates the URL for the service running locally.
-   */
-  public String getUrl() {
-    return "http://localhost:" + port + "/api";
-  }
+    /**
+     * Creates the URL for the service running locally.
+     */
+    public String getUrl() {
+        return "http://localhost:" + port + "/api";
+    }
 }
