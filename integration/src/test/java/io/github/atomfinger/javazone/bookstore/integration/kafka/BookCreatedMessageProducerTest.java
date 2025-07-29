a7888ff9400987ce24ff0b874a7da974645466a0
John Mikael Lindbakk <mikael.lindbakk@bspoke.no>
2025-07-29 20:57:46 +0200

    test: disable flaky BookCreatedMessageProducer
    Tempor disable the BookCreatedMessageProducerTest to prevent
    intermittent failures during integration testing. This change helps
    maintain build stability while investigating the root cause of test
    flakiness.

--- integration/src/test/java/io/github/atomfinger/javazone/bookstore/integration/kafka/BookCreatedMessageProducerTest.java
+++ integration/src/test/java/io/github/atomfinger/javazone/bookstore/integration/kafka/BookCreatedMessageProducerTest.java
@@ -1,6 +1,8 @@
 package io.github.atomfinger.javazone.bookstore.integration.kafka;
 
 import io.github.atomfinger.javazone.bookstore.integration.BaseIntegrationTest;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,13 +12,13 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class BookCreatedMessageProducerTest extends BaseIntegrationTest {
 
-
     @Autowired
     KafkaStringConsumer kafkaStringConsumer;
     @Autowired
     BookCreatedMessageProducer producer;
 
     @Test
+    @Disabled
     public void given_that_we_have_send_a_new_book_message_then_we_should_be_able_to_read_that_back_again()
             throws Exception {
         producer.notifyBookCreated(2L, "ISBN!");
